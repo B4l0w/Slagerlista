438344a1e00c7596f1f876c31082ca6cc4a01fa7
B4l0w <90751948+B4l0w@users.noreply.github.com>
2023-05-13 17:21:26 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,59 +11,121 @@ namespace slagerlista
     {
         static void Main(string[] args)
         {
+            //Fájl beolvasása, lista létrehozása
             string[] beolvasas = File.ReadAllLines("osszeszene.txt");
-            string[] osszeszene = new string[beolvasas.Length];
-            string[] pontszamok = File.ReadAllLines("pontszamok.txt");
             List<string> zenek = new List<string>();
 
+            //Lista feltöltése a fájlban lévő adatokkal
             for (int i = 0; i < beolvasas.Length; i++)
             {
-                osszeszene[i] = beolvasas[i];
+                zenek.Add(beolvasas[i]);
             }
 
-            string[] menupontok = { "Top10 megadása", "Top10 kilistázása"};
-            for (int i = 0; i < menupontok.Length; i++)
+            //Menüpontok tömbben
+            string[] menupontok = { "Top10 megadása", "Top10 kilistázása" , "Kilépés"};
+
+            while (true)
             {
-                Console.WriteLine($"[{i+1}] {menupontok[i]}");
-            }
-            char valasz = Console.ReadKey().KeyChar;
-            Console.Clear();
-            switch (valasz)
-            {
-                case '1':
-                    StreamWriter fajlbair = new StreamWriter("osszeszene.txt");
-                    for (int i = 0; i < 10; i++)
-                    {
-                        Console.WriteLine(i+1);
-                        Console.WriteLine("Adja meg a zene címét");
-                        string zenecim = Console.ReadLine();
-                        Console.WriteLine("Adja meg a zene szerzőjét");
-                        string szerzo = Console.ReadLine();
-                        for (int j = 0; j < osszeszene.Length; j++)
+                //Menüpontok kiíratása
+                for (int i = 0; i < menupontok.Length; i++)
+                {
+                    Console.WriteLine($"[{i + 1}] {menupontok[i]}");
+                }
+
+                char valasz = Console.ReadKey().KeyChar;
+                Console.Clear();
+
+                //Menüsor működése switchel
+                switch (valasz)
+                {
+                    case '1':
+                        //10 zene bekérése
+                        for (int i = 0; i < 10; i++)
                         {
-                            int szamlalo = int.Parse(osszeszene[i].Split('\t')[2]);
-                            if (osszeszene[i].Split('\t')[0].Contains("Zene") && osszeszene[i].Split('\t')[1].Contains("Gáspár Laci") && szamlalo == 0)
+                            //A zene és szerző bekérése
+                            Console.WriteLine($"Adja meg a(z) {i+1}. zene címét");
+                            string zenecim = Console.ReadLine();
+                            Console.WriteLine($"Adja meg a(z) {i+1}. zene szerzőjét");
+                            string szerzo = Console.ReadLine();
+
+                            //Végigmegy a listán
+                            for (int j = 0; j < zenek.Count; j++)
                             {
-                                //Akkor kikéne írni a fájlba a pontszámmal kötőjellel (-)
-                                fajlbair.WriteLine(zenecim + "\t" + szerzo + "\t" + szamlalo+1);
-                                //fajlbair.WriteLine(zenecim + " - " + szerzo);
+                                //A jelenlegi zene szavazatának száma
+                                int szamlalo = int.Parse(zenek[j].Split('-')[2]);
+
+                                //Ha már létezik a megadott zene a listában akkor törli a listából
+                                //és hozzáadja megint +1 szavazattal
+                                if (zenek[j].Contains(zenecim + " - " + szerzo))
+                                {
+                                    zenek.RemoveAt(j);
+                                    zenek.Add(zenecim + " - " + szerzo + " - " + (szamlalo + 1));
+                                    break;
+                                }
+
+                                //Ha nincs még ilyen a listában, hozzáadja 1 szavazattal
+                                if (j == zenek.Count - 1)
+                                {
+                                    szamlalo = 1;
+                                    zenek.Add(zenecim + " - " + szerzo + " - " + (szamlalo));
+                                }
                             }
-                            else
+                            Console.Clear();
+                        }
+                        
+                        //Beleírja a fájlba a frissített listát
+                        StreamWriter fajlbair = new StreamWriter("osszeszene.txt");
+                        for (int i = 0; i < zenek.Count; i++)
+                        {
+                            fajlbair.WriteLine(zenek[i]);
+                        }
+                        fajlbair.Close();
+                        break;
+
+                    case '2':
+                        try
+                        {
+                            int kiirosszamlalo = 0;
+
+                            //Top 10 rendezése
+                            var rendezettLista = zenek.OrderByDescending(zene => int.Parse(zene.Split('-')[2]));
+                            Console.WriteLine("Zene címe - Szerző - Szavazatok");
+                            Console.WriteLine();
+
+                            //Top 10 kiíratása
+                            foreach (string zene in rendezettLista)
                             {
-                                fajlbair.WriteLine(zenecim + "\t" + szerzo + "\t" + 0);
+                                kiirosszamlalo++;
+                                Console.WriteLine($"[{kiirosszamlalo}]\t{zene}");                 
+
+                                //Ha megvan a 10 kiíratott akkor leáll
+                                if (kiirosszamlalo == 10)
+                                {
+                                    break;
+                                }
                             }
                         }
-                        Console.Clear();
-                    }
-                    fajlbair.Close();
-                    break;
+                        catch (Exception)
+                        {
+                            //Ha nincs elég a top10 kiírásához ezt írja ki
+                            Console.WriteLine("Nincs elég");
+                        }
+                        break;
 
-                case '2':
+                    case '3':
+                        //Kilépés
+                        System.Environment.Exit(1);
+                        break;
 
-                default:
-                    break;
+                    default:
+                        Console.WriteLine("Nem létezik ilyen menüpont!");
+                        break;
+                }
+                Console.SetCursorPosition(Console.WindowWidth / 2 - 17, Console.WindowHeight / 2);
+                Console.WriteLine("Nyomj meg egy gombot a továbblépéshez");
+                Console.ReadKey();
+                Console.Clear();
             }
-            Console.ReadLine();
         }
     }
 }
